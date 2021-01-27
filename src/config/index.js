0a2a32bf865737fd85eb93634a05eb0180fa9ddd
Andrey Khomyakov <yourwebwizard@gmail.com>
2021-01-27 18:20:56 +0300

    config

--- src/config/index.js
+++ src/config/index.js
@@ -1,5 +1,6 @@
 const config = {
   env: process.env.NODE_ENV,
+  backendEndpoint: process.env.REACT_APP_BACKEND_ENDPOINT,
   googleRedirectURI: process.env.REACT_APP_GOOGLE_REDIRECT_URI,
   facebookRedirectURI: process.env.REACT_APP_FACEBOOK_REDIRECT_URI,
 };
--- src/services/authService.js
+++ src/services/authService.js
@@ -1,13 +1,14 @@
 import axios from "../utils/axios";
+import config from "../config";
 
 axios.defaults.withCredentials = true;
-axios.defaults.baseURL = "http://localhost:8080/";
+axios.defaults.baseURL = config.backendEndpoint;
 
 export function signIn(credentials) {
   return new Promise((resolve, reject) => {
-    axios.get("sanctum/csrf-cookie").then(() => {
+    axios.get("/sanctum/csrf-cookie").then(() => {
       axios
-        .post("api/login", credentials)
+        .post("/api/login", credentials)
         .then((response) => {
           if (response.status === 200) {
             resolve(response.data);
@@ -24,9 +25,9 @@ export function signIn(credentials) {
 export function signUp(credentials) {
   console.log(axios.defaults.baseURL);
   return new Promise((resolve, reject) => {
-    axios.get("sanctum/csrf-cookie").then(() => {
+    axios.get("/sanctum/csrf-cookie").then(() => {
       axios
-        .post("api/register", credentials)
+        .post("/api/register", credentials)
         .then((response) => {
           console.log(response.data);
           if (response.status === 200) {
