01a7787c641100c3a8477c8a7619582c34e11c3c
Andrey Khomyakov <yourwebwizard@gmail.com>
2021-01-27 15:16:59 +0300

    auth

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,39 +1,43 @@
 import axios from "../utils/axios";
 
-axios.defaults.baseURL = "http://localhost:8080/api/";
+axios.defaults.withCredentials = true;
+axios.defaults.baseURL = "http://localhost:8080/";
 
 export function signIn(credentials) {
   return new Promise((resolve, reject) => {
-    axios
-      .post("/api/auth/sign-in", credentials)
-      .then((response) => {
-        if (response.status === 200) {
-          resolve(response.data);
-        }
-        reject(response.data);
-      })
-      .catch((error) => {
-        reject(error);
-      });
+    axios.get("sanctum/csrf-cookie").then(() => {
+      axios
+        .post("api/login", credentials)
+        .then((response) => {
+          if (response.status === 200) {
+            resolve(response.data);
+          }
+          reject(response.data);
+        })
+        .catch((error) => {
+          reject(error);
+        });
+    });
   });
 }
 
 export function signUp(credentials) {
   console.log(axios.defaults.baseURL);
   return new Promise((resolve, reject) => {
-    axios
-      .post("register", credentials)
-      //.post("/api/auth/sign-up", credentials)
-      .then((response) => {
-        console.log(response.data);
-        if (response.status === 200) {
-          resolve(response.data);
-        }
-        reject(response.data);
-      })
-      .catch((error) => {
-        reject(error);
-      });
+    axios.get("sanctum/csrf-cookie").then(() => {
+      axios
+        .post("api/register", credentials)
+        .then((response) => {
+          console.log(response.data);
+          if (response.status === 200) {
+            resolve(response.data);
+          }
+          reject(response.data);
+        })
+        .catch((error) => {
+          reject(error);
+        });
+    });
   });
 }
 
