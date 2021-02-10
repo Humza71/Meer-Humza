b7d2d1f32a1ad47c5c669f2a0ffb0872ff3daa87
visisemanaj <ervis.semanaj@outlook.com>
2021-02-10 10:11:30 -0500

    remove unnecessary things

--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -1,18 +1,3 @@
-// import { createStore, applyMiddleware } from "redux";
-// import { composeWithDevTools } from "redux-devtools-extension";
-// import thunk from "redux-thunk";
-
-// import { rootReducer } from "../reducers/index";
-
-// const composeEnhancers = composeWithDevTools({});
-
-// const store = createStore(
-//   rootReducer,
-//   composeEnhancers(applyMiddleware(thunk))
-// );
-
-// export default store;
-
 import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
 import logger from "redux-logger";
 import { routerMiddleware } from "connected-react-router";
